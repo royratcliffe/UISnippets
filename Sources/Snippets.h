db51a641ab81407e317efed11a9eb5379d0fe6c6
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2017-04-18 11:36:53 +0100

    Import Foundation, not UIKit

--- Sources/Snippets.h
+++ Sources/Snippets.h
@@ -22,7 +22,7 @@
  *
  ******************************************************************************/
 
-@import UIKit;
+@import Foundation;
 
 FOUNDATION_EXPORT double SnippetsVersionNumber;
 FOUNDATION_EXPORT const unsigned char SnippetsVersionString[];
